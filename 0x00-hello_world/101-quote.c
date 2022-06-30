301b6a44f8ea2745bfe641581c7399821b24874d
adeeex1 <adanefilate@gmail.com>
2022-06-30 07:36:24 -0700

    write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 1
+ */
+int main(void)
+{
+	write(1, "and that piece of art is usful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
