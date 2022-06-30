56b84de9d062626c2fd78ed14cfcbaf19d733237
adeeex1 <adanefilate@gmail.com>
2022-06-30 05:50:25 -0700

    wall printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: ALways 0
+ */
+int main(void)
+{
+	prinf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
