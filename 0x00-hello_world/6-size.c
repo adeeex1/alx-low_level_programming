dcd16c570255724aca8095cb59f119d8e2611fb1
adeeex1 <adanefilate@gmail.com>
2022-06-30 06:20:56 -0700

    size of varius computer

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: ALways 0
+ */
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n");
+	printf("Size of an init: %zu byte(s)\n");
+	printf("Size of a long int: %zu byte(s)\n");
+	printf("Size of a long long int: %zu byte(s)\n");
+	printf("Size of a float: %zu byte(s)\n");
+	return (0);
+}
