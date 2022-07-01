ab42f47b071ae0cf838ef6c314ffb702dbc69e6f
adeeex1 <adanefilate@gmail.com>
2022-07-01 13:38:44 -0700

    comb 5\

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 0; num1 < 99; num1++)
+	{
+		for (num2 = num1 + 1; num2 < 100; num2++)
+		{
+			putchar(num1 + '0');
+			putchar(num1 + '0');
+			putchar(' ');
+			putchar(num2 + '0');
+			putchar(num2 + '0');
+
+			if (num1 < 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
