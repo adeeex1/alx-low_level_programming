a55382ded17ad49eb3b39cd897fd2bbd2974e4e9
adeeex1 <adanefilate@gmail.com>
2022-07-01 13:00:36 -0700

    print two number

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 0; num1 < 9; num1++)
+	{
+		for (num2 = num1 +'1'; num2 < 10; num2++)
+		{
+			putchar(num1 + '0');
+			putchar(num2 + '0');
+
+			if (num1 < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
