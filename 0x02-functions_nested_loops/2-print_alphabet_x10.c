26d131baa0e103949fc7b557c7e314714fc33e0e
adeeex1 <adanefilate@gmail.com>
2022-07-14 13:45:02 -0700

    10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,9 @@ void print_alphabet_x10(void)
 {
 	int i, a;
 
-	for(i = 0; i <=9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for(a = 'a'; a <= 'z'; a++)
+		for (a = 'a'; a <= 'z'; a++)
 		{
 			_putchar(a);
 		}
