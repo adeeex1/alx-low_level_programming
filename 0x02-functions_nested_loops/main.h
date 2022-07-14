386b273f1d729315c6f24ffe36779baff8b0eef3
adeeex1 <adanefilate@gmail.com>
2022-07-14 13:16:06 -0700

    header

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * file: main.h
+ *
+ * desc: header file containing declarations for all function
+ */
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+
+#endif
