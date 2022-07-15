a8ad74fd7212946db35c59a0efc614694915bd6c
adeeex1 <adanefilate@gmail.com>
2022-07-15 12:06:45 -0700

    string to code

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * leet - entry point
+ * @s: enter value
+ *
+ * Return: void
+ */
+char *leet(char *s)
+{
+	int i, j, k;
+
+	char l[] = "oOlLeEaAtT";
+	char e[] = "0011334477";
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		k = 0;
+		while (l[j] != '\0')
+		{
+			if (s[i] == l[j])
+			{
+				k = j;
+				s[i] = e[k];
+			}
+			j++;
+		}
+		i++;
+	}
+	return (s);
+}
