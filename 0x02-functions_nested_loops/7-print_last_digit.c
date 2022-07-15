2566c395079d563a1dd9f1fb0a163a02ec19a9e9
adeeex1 <adanefilate@gmail.com>
2022-07-15 04:34:12 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,15 +10,15 @@ int print_last_digit(int n)
 {
 	int a;
 
-	if (n < 0)
-	{
-		n = -n;
-		a = n % 10;
-	}
+	a = n % 10;
 	if (a < 0)
 	{
-		a = -a;
-		_putchar (a + '0');
+		_putchar(-l + 48);
+		return (-l);
 	}
-	return (a);
+	else
+	{
+		_putchar(l + 48);
+		return (l);
+	}
 }
