34cb88fa91e239f0518e05c4a3119021fb901aba
adeeex1 <adanefilate@gmail.com>
2022-07-15 05:18:47 -0700

    adds two intiger

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two intiger and return the result
+ * @a: int added to int b
+ * @b: int added to int a
+ *
+ * Return: always 0
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
